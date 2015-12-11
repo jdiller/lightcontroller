addbd6210431a43a50aa7be5f92ea6ac79bae57b
Jason Diller <jdiller@pagerduty.com>
2015-12-11 18:43:20 -0500

    Get rid of flashing property, use transitions to new settings instead

--- lightcontroller/lightsettings.py
+++ lightcontroller/lightsettings.py
@@ -5,28 +5,24 @@ import json
 class LightSettingsEncoder(json.JSONEncoder):
 
     def default(self, s):
-        if isinstance(s, LightSettings):
-            ret = {}
-            props = ['red', 'green', 'blue',
-                     'on_duration', 'next_settings']
-            for p in props:
-                if (getattr(s, p) is not None):
-                    ret[p] = getattr(s, p)
-            return ret
-        else:
-            return json.JSONEncoder.default(self, s)
+        ret = {}
+        props = ['red', 'green', 'blue',
+                 'on_duration', 'next_settings']
+        for p in props:
+            if (getattr(s, p) is not None):
+                ret[p] = getattr(s, p)
+        return ret
 
 
 class LightSettings(object):
 
-    def __init__(self, red=None, blue=None, green=None, flashing=None, on_duration=None, color=None):
+    def __init__(self, red=None, blue=None, green=None, on_duration=None, color=None):
         if color and (red or blue or green):
             raise ValueError(
                 'Set individual colors or give a color tuple, not both')
         self._red = red
         self._blue = blue
         self._green = green
-        self._flashing = flashing
         self._on_duration = on_duration
         self.next_settings = None
         self._transition_time = None
@@ -62,24 +58,6 @@ class LightSettings(object):
         if value is not None and (value < 0 or value > 255):
             raise ValueError('Intensity must be between 0 and 255')
         self._blue = value
-
-    @property
-    def flashing(self):
-        return self._flashing
-
-    @flashing.setter
-    def flashing(self, value):
-        if isinstance(value, str):
-            self._flashing = value.lower() in ('yes', 'true', 't', '1')
-        elif isinstance(value, unicode):
-            self._flashing = value.lower() in (u'yes', u'true', u't', u'1')
-        else:
-            self._flashing = (value == True)
-        if self._flashing:
-           next_settings = LightSettings()
-           next_settings.all_off()
-           next_settings.flashing = True
-           next_settings.next_settings = self
 
     @property
     def transition_time(self):
@@ -121,7 +99,6 @@ class LightSettings(object):
         ret.blue = d.get('blue')
         ret.red = d.get('red')
         ret.green = d.get('green')
-        ret.flashing = d.get('flashing')
         ret.on_duration = d.get('on_duration')
         return ret
 
--- tests/testlightsettings.py
+++ tests/testlightsettings.py
@@ -32,19 +32,6 @@ class TestLightSettings(unittest.TestCase):
         s.green = 80
         self.assertEquals(s.green, 80)
 
-    def test_can_set_flashing(self):
-        s = LightSettings()
-        s.flashing = True
-        self.assertTrue(s.flashing)
-        s.flashing = 1
-        self.assertTrue(s.flashing)
-        s.flashing = "Yes"
-        self.assertTrue(s.flashing)
-        s.flashing = "yes"
-        self.assertTrue(s.flashing)
-        s.flashing = "1"
-        self.assertTrue(s.flashing)
-
     def test_invalid_red_value_throws(self):
         s = LightSettings()
         with self.assertRaises(ValueError):
@@ -79,23 +66,21 @@ class TestLightSettings(unittest.TestCase):
 
     def test_can_initialize_via_constructor(self):
         s = LightSettings(
-            red=1, blue=2, green=3, flashing=True, on_duration=99)
+            red=1, blue=2, green=3, on_duration=99)
         self.assertEquals(s.red, 1)
         self.assertEquals(s.blue, 2)
         self.assertEquals(s.green, 3)
-        self.assertTrue(s.flashing)
         self.assertEquals(s.on_duration, 99)
 
     def test_can_load_from_json(self):
         d = {'red': 10, 'blue': 20, 'green': 30,
-             'flashing': 'yes', 'on_duration': 1}
+             'on_duration': 1}
         json_str = json.dumps(d)
         s = LightSettings.from_json(json_str)
         self.assertTrue(s is not None)
         self.assertEquals(s.red, 10)
         self.assertEquals(s.blue, 20)
         self.assertEquals(s.green, 30)
-        self.assertTrue(s.flashing)
         self.assertEquals(s.on_duration, 1)
 
     def test_can_iterate_led_settings(self):
@@ -106,11 +91,6 @@ class TestLightSettings(unittest.TestCase):
         for led, val in s.leds:
             self.assertTrue(led in [17, 18, 19])
             self.assertEqual(val, 20)
-
-    def test_durations_not_none_when_flash_is_on(self):
-        s = LightSettings(red=10)
-        s.flashing = True
-        self.assertTrue(s.on_duration is not None)
 
     def test_can_create_from_color_tuple(self):
         color = (255, 255, 255)
