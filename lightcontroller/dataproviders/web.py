299548194a4512854cddc3c3a6e7536057c86131
Jason Diller <jason@diller.ca>
2016-02-28 00:44:39 -0500

    Some basic webpolling plugin

---
+++ lightcontroller/dataproviders/web.py
@@ -0,0 +1,26 @@
+import requests
+import logging
+from ConfigParser import ConfigParser
+
+
+class WebPollData(object):
+
+    def __init__(self):
+        cp = ConfigParser()
+        cp.read('config.cfg')
+        self.url = cp.get('WebPoll', 'url')
+        self.data = {}
+
+    @property
+    def request_url(self):
+        return self.url
+
+    def refresh(self):
+        logging.debug("Refreshing WebPoll data")
+        resp = requests.get(self.request_url)
+        self.data = resp.json()
+
+    def get(self, value, default=None):
+        if self.data is None:
+            self.refresh()
+        return self.data.get(value, default)
--- lightcontroller/plugins/webpoll.py
+++ lightcontroller/plugins/webpoll.py
@@ -1,17 +1,23 @@
 import requests
+import logging
+from lightcontroller.plugins.plugin import Plugin
+from lightcontroller.dataproviders.web import WebPollData
 
+class WebPoll(Plugin):
+    def _apply_settings(self, settings):
+        data = WebPollData()
+        data.refresh()
+        red = data.get('red', None)
+        blue = data.get('blue', None)
+        green = data.get('green', None)
 
-class WebEndpoint(object):
+        if red or blue or green:
+            logging.debug("Got web poll data. Applying to settings")
+            settings.red = data.get('red', 0)
+            settings.blue = data.get('blue', 0)
+            settings.green = data.get('green', 0)
+            settings.transition_time = 2
+            settings.next_settings = None
+        else:
+            logging.debug("No data from web poll, passing through settings unchanged")
 
-    def __init__(self, base_url):
-        self.base_url = base_url
-        self.data = None
-
-    def refresh(self, params=[]):
-        r = requests.get(self.base_url.format(params))
-        self.data = r.json()
-
-    def get(self, value, default=None):
-        if not self.data:
-            self.refresh()
-        return self.data.get(value, default)
